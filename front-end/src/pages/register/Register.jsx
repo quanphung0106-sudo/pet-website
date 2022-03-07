59bdd571afc76dce26c0a18650edda00f6dac00e
quanphung0106-sudo <phungdinhquan16@gmail.com>
2022-03-07 11:18:31 +0700

    updated register page

--- front-end/src/pages/register/Register.jsx
+++ front-end/src/pages/register/Register.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Container, Row, Col, Form, InputGroup } from "react-bootstrap";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
+import "./register.css";
 
 export default function Register() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
+
+  const hideOrShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const hideOrShowPassword2 = () => {
+    setShowPassword2(!showPassword2);
+  };
+
   return (
     <div>
-        <h1>This is Register Page!</h1>
+      <Container fluid>
+        <Row className="register-page gy-5">
+            
+          {/* <div className="register-page__image position-relative">
+              <img src="./assets/images/signup-signin-dog.jpg" alt="register-image" />
+            </div> */}
+          <Col className="d-flex justify-content-center align-items-center">
+            <Form className="register-page__register-form br-6">
+              <div className="register-page__register-form--padding">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    className="br-6"
+                    type="email"
+                    placeholder="Enter your email"
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>User name</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter your user name"
+                    className="br-6"
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <InputGroup className="position-relative">
+                    <Form.Control
+                      type={`${showPassword ? "text" : "password"}`}
+                      placeholder="Enter your password"
+                      className="br-6"
+                    />
+                    <div
+                      className="position-absolute"
+                      style={{ right: "10px", zIndex: 10, padding: "11px" }}
+                    >
+                      {showPassword ? (
+                        <FaEyeSlash
+                          className="eyes-btn"
+                          onClick={() => hideOrShowPassword()}
+                        />
+                      ) : (
+                        <FaEye
+                          className="eyes-btn"
+                          onClick={() => hideOrShowPassword()}
+                        />
+                      )}
+                    </div>
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formBasicCheckbox"
+                ></Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <InputGroup className="position-relative">
+                    <Form.Control
+                      type={`${showPassword2 ? "text" : "password"}`}
+                      placeholder="Confirm your password"
+                      className="br-6"
+                    />
+                    <div
+                      className="position-absolute"
+                      style={{ right: "10px", zIndex: 10, padding: "11px" }}
+                    >
+                      {showPassword2 ? (
+                        <FaEyeSlash
+                          className="eyes-btn"
+                          onClick={() => hideOrShowPassword2()}
+                        />
+                      ) : (
+                        <FaEye
+                          className="eyes-btn"
+                          onClick={() => hideOrShowPassword2()}
+                        />
+                      )}
+                    </div>
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formBasicCheckbox"
+                ></Form.Group>
+                <Button
+                  variant="dark"
+                  type="submit"
+                  className="br-6"
+                  style={{ width: "100%", padding: "12px", marginTop: "12px" }}
+                >
+                  Register
+                </Button>
+              </div>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }

6584852f0a6779e0801ffe285de1384930b86576
Bao Cong <ngobaocong.it@gmail.com>
2022-06-01 14:53:00 +0700

    import

--- front-end/src/components/SingleProduct/Product.js
+++ front-end/src/components/SingleProduct/Product.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { ProductDetailContext } from "../HomePage/store/Context";
 

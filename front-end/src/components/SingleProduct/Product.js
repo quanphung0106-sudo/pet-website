9d9a614e19f2ea0a0f1d77ec10b5fc346226a0c4
Bao Cong <ngobaocong.it@gmail.com>
2022-06-03 09:04:52 +0700

    update toastify

--- front-end/src/components/SingleProduct/Product.js
+++ front-end/src/components/SingleProduct/Product.js
@@ -25,7 +25,15 @@ const Product = ({ user, axiosJWT }) => {
   }, [path]);
 
   const handleAddProductToCart = () => {
-    toast.success("1 bé đã được thêm vào giỏ hàng!")
+    toast.success('1 bé đã được thêm vào giỏ hàng!', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      });
     if (user) {
       try {
         axiosJWT
